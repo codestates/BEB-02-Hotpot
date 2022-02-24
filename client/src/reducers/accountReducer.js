1f3c55a7bf0e01bc3397c0ee3ceed701bc3f6c4b
PCY71 <skypcy71@gmail.com>
2022-02-24 10:33:13 +0900

    add login/logout feat & redux

--- client/src/reducers/accountReducer.js
+++ client/src/reducers/accountReducer.js
@@ -1,8 +1,24 @@
-import { initialState } from './initialState';
+import { LOG_IN, LOG_OUT } from '../actions/index'
+
+
+const initialState = {
+    "account": {
+        "_id": "",
+        "username": "",
+        "password": "",
+        "date": "",
+        "_v": ""
+    }, "isLogin": false
+}
 
 const accountReducer = (state = initialState, action) => {
     switch (action.type) {
-
+        case LOG_IN:
+            return action.payload;
+        case LOG_OUT:
+            return initialState;
+        default:
+            return state;
     }
 }
 
