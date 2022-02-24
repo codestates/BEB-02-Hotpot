2b03025fa21bfe5a7a1e208c8a9481ff04228d7c
PCY71 <skypcy71@gmail.com>
2022-02-24 17:52:48 +0900

    add pw crypto

--- server/routes/account.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { accountController } = require('../controller')
-
-// POST /account/login
-//todo
-//logout - post
-//todo
