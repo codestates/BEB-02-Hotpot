59b6e0f8660f2331dc5dfb5d62e37b5bd8cd4777
koeunchorong <koasis03@gmail.com>
2022-03-04 13:54:23 +0900

    토큰 전송 코드 수정

--- server/routes/reward.js
+++ server/routes/reward.js
@@ -4,6 +4,7 @@ const crypto = require("crypto-js");
 const ethFaucetForm = require("../forms/ethFaucetForm");
 const Web3 = require("web3");
 const erc20Abi = require("../erc20Abi");
+
 const tokenAddress = "0x019A351B33D2e7a5124065a7738b3423482864D9"; //HPT 토큰주소
 let web3, serverAddress, contract;
 let balance = 0;
@@ -22,13 +23,16 @@ async function getBalance(toAddress) {
 
 //컨트랙트의 토큰 transfer (코드스테이츠 제공)
 async function setTransfer(fromAddress, toAddress, amount) {
-  const isOk = await contract.methods.approve(toAddress, parseInt(amount));
-  //console.log("토큰 승인 요청 성공여부 : ", isOk);
+  const isOk = await contract.methods.approve(
+    toAddress,
+    web3.utils.toHex(amount)
+  );
+  console.log("토큰 승인 요청 성공여부 : ", isOk);
   const limited_token = await contract.methods.approve(fromAddress, toAddress);
-  // console.log(`전송이 허용된 토큰수량 : ${limited_token}개 입니다.`);
-  // console.log('transfer() 실행전....');
+  console.log(`전송이 허용된 토큰수량 : ${limited_token}개 입니다.`);
+  console.log("transfer() 실행전....");
   const result = await contract.methods
-    .transfer(toAddress, parseInt(amount))
+    .transfer(toAddress, web3.utils.toHex(amount))
     .send(
       {
         from: fromAddress,
@@ -54,24 +58,83 @@ async function setTransfer(fromAddress, toAddress, amount) {
 async function setTransferFrom(fromAddress, toAddress, amount) {
   let nonce = web3.utils.toHex(web3.eth.getTransactionCount(fromAddress));
   let gasPrice = web3.utils.toHex(web3.eth.gasPrice);
-  let value = web3.utils.toHex(web3.utils.toWei(`${amount}`));
-  console.log(value);
+  let value = web3.utils.toHex(web3.utils.toWei(`${amount}`, "ether"));
+  console.log("전송금액:", value);
   let gasLimit = web3.utils.toHex(100000);
 
   const isOk = await contract.methods.approve(toAddress, value);
   console.log("토큰 승인 요청 성공여부 : ", isOk);
-  const limited_token = await contract.methods.approve(fromAddress, toAddress);
+  const limited_token = await contract.methods.approve(fromAddress, "1000");
   console.log(`전송이 허용된 토큰수량 : ${limited_token}개 입니다.`);
-  const result = await contract.methods.transferFrom(
-    fromAddress,
-    toAddress,
-    value
-  );
+  const result = await contract.methods
+    .transferFrom(fromAddress, toAddress, "1000")
+    .send({
+      from: fromAddress,
+      //gasLimit: web3.utils.toHex(100000),
+      gasPrice: web3.utils.toHex(web3.eth.gasPrice),
+      gas: web3.utils.toHex(1000000),
+    });
+  // .send({from: fromAddress, gasPrice: web3.utils.toHex(web3.eth.gasPrice), gas: web3.utils.toHex(100000)})
+  // .on("receipt", (receipt) => {
+  //     setTo("");
+  // })
   return result;
 }
 
+async function setTransferSigned(server, fromAddress, toAddress, amount) {
+  let gasPrice = web3.utils.toHex(web3.eth.gasPrice);
+  //let value = web3.utils.toWei(amount, "ether");
+  let gasLimit = web3.utils.toHex(
+    web3.eth.estimateGas({
+      to: toAddress,
+      from: fromAddress,
+      value: amount,
+    })
+  );
+  //5.보낼 트랜잭션 데이터 생성
+  const data = contract.methods.transfer(fromAddress, amount).encodeABI();
+  try {
+    let userBalance = await contract.methods.balanceOf(toAddress).call();
+    console.log(`======== 토큰 발송 전 보유량 ========`);
+    console.log(`[전송 전] user 토큰 수 : ${userBalance}`);
+
+    //6.트랜잭션에 server 서명
+    const signedTx = await web3.eth.accounts.signTransaction(
+      {
+        to: tokenAddress, //받는 사람이 아니라, 계약주소임. 받는 사람은 transfer 함수에 설정
+        gas: gasPrice,
+        gasLimit: gasLimit,
+        data: data,
+      },
+      server.privateKey
+    );
+
+    //7.트랜잭션 발송
+    const rtnTran = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction
+    );
+    console.log("sendSignedTraction 리턴값 : ", rtnTran);
+    userBalance = await contract.methods.balanceOf(toAddress).call();
+    //let allowance = await contractObj.methods.allowance(userAddress, serverAddress).call();
+    let allowance = await contract.methods
+      .allowance(toAddress, tokenAddress)
+      .call();
+    console.log(
+      `======== 트랜잭션 결과(토큰 발행 및 apporve [user][server]) ========`
+    );
+    console.log(`[전송 후] to 토큰 수 : ${userBalance}`);
+    console.log(`[전송 후] allowance[user][server] 토큰 수 : ${allowance}`);
+  } catch (e) {
+    console.log(e);
+    // res.status(502).json({
+    //     message: "Error: signTransaction Failed",
+    // });
+  }
+}
+
 router.post("/reward", (req, res) => {
-  const { useraddress } = req.body;
+  const { username, useraddress } = req.body;
+  //console.log(username, useraddress);
 
   // users 테이블(서버의 계정주소와 프라이빗키가 저장되어 있는 테이블) 검색
   // 현재 username과 패스워드는 server, server 이다.
@@ -82,14 +145,13 @@ router.post("/reward", (req, res) => {
         "https://ropsten.infura.io/v3/3fbe081021374f6d876b8153dab168f5"
       );
       await web3.eth.accounts.wallet.add(server.privateKey);
-      console.log(server.privateKey);
+      console.log("프라이빗키", server.privateKey);
       //2.서버 계정 주소
       serverAddress = server.address;
       console.log("서버(토큰생성자) 계정 주소 : ", serverAddress);
 
       //3. 사용자(토큰받을 사람)의 계정주소 useraddress
-      let toAddress = useraddress;
-      console.log("3. 사용자(토큰받을 사람)의 계정주소", useraddress);
+      let toAddress = "0x11F4190bA6a09c165f468242568fD17C25139516";
 
       //4.스마트 컨트랙트 객체 생성 tokenAddress가 스마트컨트랙주소???
       contract = new web3.eth.Contract(erc20Abi, tokenAddress, {
@@ -100,14 +162,20 @@ router.post("/reward", (req, res) => {
       const bal = await getBalance(serverAddress);
       console.log(`지갑 ${serverAddress}의 잔액은... ${bal} wei입니다.`);
       balance = web3.utils.fromWei(bal);
-      let amount = 4;
+      let amount = "4770000";
       let result;
       if (balance > 0.0) {
         //let toAddress = serverAddress;
-        result = await setTransferFrom(serverAddress, toAddress, 4).then(() => {
-          console.log(`4 HPT 전송.`);
-          //console.log(`총잔액 ${balance} HPT`);
-        });
+        result = await setTransferSigned(
+          server,
+          serverAddress,
+          toAddress,
+          amount
+        );
+        // .then(() => {
+        //     console.log(`HPT 전송.`);
+        //     //console.log(`총잔액 ${balance} HPT`);
+        // });
       } else {
         console.log(`지갑 잔액부족. ${balance} HPT입니다.`);
       }
