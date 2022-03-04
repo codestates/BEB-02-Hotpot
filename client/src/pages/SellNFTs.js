bcd8294b9ed3f7b7dfd9b5d0ce1fbd88ee7c1324
sungyeon1 <sungyeon0320@gmail.com>
2022-03-05 00:10:11 +0900

    판매 시 관리자에게 권한 양도 및 exchange에 토큰 등록

--- client/src/pages/SellNFTs.js
+++ client/src/pages/SellNFTs.js
@@ -6,118 +6,112 @@ import HotNFTAbi from "../HotNFTAbi";
 import SellNFT from "../components/SellNFT";
 
 const SellNFTs = ({ web3 }) => {
-    // 일단은 컨트랙트 하드코딩한거 나중에 변경될수도
-    const NFTaddr = "0xCc1108f7c353e5d12DfdeD369ec21C46570afd6D";
-    const account = useSelector((state) => state.accountReducer);
-    const nfts = useSelector((state) => state.myNFTReducer);
-    const [isLoading, setisLoading] = useState(true);
-    const dispatch = useDispatch();
+  // 일단은 컨트랙트 하드코딩한거 나중에 변경될수도
+  const NFTaddr = "0xCc1108f7c353e5d12DfdeD369ec21C46570afd6D";
+  const account = useSelector((state) => state.accountReducer);
+  const nfts = useSelector((state) => state.myNFTReducer);
+  const [isLoading, setisLoading] = useState(true);
+  const dispatch = useDispatch();
 
-    const findNFTs = async () => {
-        const tokenContract = await new web3.eth.Contract(HotNFTAbi, NFTaddr, {
-            from: account.account.address,
-        });
-        const name = await tokenContract.methods.name().call();
-        const symbol = await tokenContract.methods.symbol().call();
-        const totalSupply = await tokenContract.methods.totalSupply().call();
+  const findNFTs = async () => {
+    const tokenContract = await new web3.eth.Contract(HotNFTAbi, NFTaddr, {
+      from: account.account.address,
+    });
+    const name = await tokenContract.methods.name().call();
+    const symbol = await tokenContract.methods.symbol().call();
+    const totalSupply = await tokenContract.methods.totalSupply().call();
 
-        let arr = [];
-        for (let i = 1; i <= totalSupply; i++) {
-            arr.push(i);
-        }
-        const nfts = [];
-
-        for (let tokenId of arr) {
-            let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
-            if (String(tokenOwner).toLowerCase() === String(account.account.address).toLowerCase()) {
-                let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
-                const nft = {
-                    tokenContract,
-                    name,
-                    symbol,
-                    tokenId,
-                    tokenURI
-                }
-                nfts.push(nft);
-            }
-        }
-        dispatch(loadMyNFTs(nfts))
-            .then(setisLoading(false));
+    let arr = [];
+    for (let i = 1; i <= totalSupply; i++) {
+      arr.push(i);
     }
+    const nfts = [];
 
-    useEffect(() => {
-        dispatch(loadMyNFTs([]));
-        findNFTs();
-    }, []);
+    for (let tokenId of arr) {
+      let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
+      if (
+        String(tokenOwner).toLowerCase() ===
+        String(account.account.address).toLowerCase()
+      ) {
+        let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
+        const nft = {
+          tokenContract,
+          name,
+          symbol,
+          tokenId,
+          tokenURI,
+        };
+        nfts.push(nft);
+      }
+    }
+    dispatch(loadMyNFTs(nfts)).then(setisLoading(false));
+  };
 
-    return (
-        <Container>
-            <Body>
-                <Title>Sell your NFT!</Title>
-                <Wrapper>
-                    {nfts.nfts.length === 0 ? (
-                        <NFTListText>
-                            보유한 nft가 없습니다.
-                        </NFTListText>
-                    ) : (
-                        <NFTList>
-                            {nfts.nfts.map((nft, idx) => {
-                                return <SellNFT
-                                    key={idx}
-                                    idx={idx}
-                                    nft={nft}
-                                />
-                            })
+  useEffect(() => {
+    dispatch(loadMyNFTs([]));
+    findNFTs();
+  }, []);
 
-                            }
-                        </NFTList>
-                    )}
-                </Wrapper>
-            </Body >
-        </Container>
-    )
-
-}
-
+  return (
+    <Container>
+      <Body>
+        <Title>Sell your NFT!</Title>
+        <Wrapper>
+          {isLoading ? (
+            <NFTListText>Loading...</NFTListText>
+          ) : nfts.nfts.length === 0 ? (
+            <NFTListText>보유한 nft가 없습니다.</NFTListText>
+          ) : (
+            <NFTList>
+              {nfts.nfts.map((nft, idx) => {
+                return <SellNFT key={idx} nft={nft} web3={web3} />;
+              })}
+            </NFTList>
+          )}
+        </Wrapper>
+      </Body>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-display: table;
-margin: 0 auto;
-width: 100%;
-height: calc(100vh - 4rem);
+  display: table;
+  margin: 0 auto;
+  width: 100%;
+  height: calc(100vh - 4rem);
 `;
 
 const Body = styled.div`
-background-color: #bc9eff0e;
-padding: 5rem;
-display: table-cell;
-width: 100%;
+  background-color: #bc9eff0e;
+  padding: 5rem;
+  display: table-cell;
+  width: 100%;
 `;
 
 const Title = styled.div`
-font-size: 1.5rem;
-font-weight: 800;
-padding-bottom: 1rem;
-margin: 0.5rem;
+  font-size: 1.5rem;
+  font-weight: 800;
+  padding-bottom: 1rem;
+  margin: 0.5rem;
 `;
 
 const Wrapper = styled.div`
-display: flex;
+  display: flex;
 `;
 
 const NFTListText = styled.div`
-text-align: center;
-padding: 10rem;
-font-size: 1.3rem;
-font-weight: 800;
-color: #4000c7;
-width: 800px;
-display: inline-block;
+  text-align: center;
+  padding: 10rem;
+  font-size: 1.3rem;
+  font-weight: 800;
+  color: #4000c7;
+  width: 800px;
+  display: inline-block;
 `;
 
 const NFTList = styled.div`
-flex: 1 1 auto;
-margin: 1rem;
+  flex: 1 1 auto;
+  margin: 1rem;
 `;
 
 export default SellNFTs;
